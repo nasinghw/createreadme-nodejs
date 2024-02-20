57802585822f79ca67d781f53713111c8db67c4a
nasinghw <151625048+nasinghw@users.noreply.github.com>
2024-02-20 18:16:53 +0000

    questions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require('path');
-const inquirer = require("in?uirer");
+const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of ?uestions for user
@@ -8,29 +8,29 @@ const questions = [
     {
         type: 'input',
         name: 'title?',
-        message: 'What is your project title?'
+        message: 'What is title of your project?'
     },
     {
         type: 'input',
         name: 'description?',
-        message: 'Please enter a description'
+        message: 'Please give project description'
     },
 
     {
         type: 'input',
         name: 'installation?',
-        message: 'Please enter some installation instructions'
+        message: 'Please provide user instructions for installation'
     },
     {
         type: 'input',
         name: 'usage?',
-        message: 'Please enter some usage instructions'
+        message: 'Please provide usage instructions'
     },
     {
         type: 'checkbox',
         name: 'license?',
-        message: 'Please choose your license',
-        choices: ['MIT', 'The Unlicense']
+        message: 'Choose license',
+        choices: ['MIT', 'Unlicensed']
     },
     {
         type: 'input',
