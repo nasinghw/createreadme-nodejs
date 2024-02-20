948eea84c5d9eb26a6b35c746cca70eadd9f2711
nasinghw <151625048+nasinghw@users.noreply.github.com>
2024-02-20 18:11:52 +0000

    questions

--- index.js
+++ index.js
@@ -5,6 +5,58 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'titleQ',
+        message: 'What is your project title?'
+    },
+    {
+        type: 'input',
+        name: 'descriptionQ',
+        message: 'Please enter a description'
+    },
+
+    {
+        type: 'input',
+        name: 'installationQ',
+        message: 'Please enter some installation instructions'
+    },
+    {
+        type: 'input',
+        name: 'usageQ',
+        message: 'Please enter some usage instructions'
+    },
+    {
+        type: 'checkbox',
+        name: 'licenseQ',
+        message: 'Please choose your license',
+        choices: ['MIT', 'The Unlicense']
+    },
+    {
+        type: 'input',
+        name: 'contributingQ',
+        message: 'Please enter any contributers'
+    },
+    {
+        type: 'input',
+        name: 'testsQ',
+        message: 'Please enter some test information'
+    },
+    {
+        type: 'input',
+        name: 'githubQ',
+        message: 'Please enter your github username'
+    },
+    {
+        type: 'input',
+        name: 'emailQ',
+        message: 'Please enter your email address'
+    },
+    {
+        type: 'input',
+        name: 'fullNameQ',
+        message: 'Please enter your full name'
+    }
 
 ];
 
